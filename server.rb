19f70a4c7f9a3d6a56f7658ec74c61c7302977e0
Olivia <oereeve@gmail.com>
2015-11-17 16:58:25 -0500

    Andrew is still the best <3

--- server.rb
+++ server.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'csv'
+require 'pry'
 
 set :views_folder, File.join(File.dirname(__FILE__), "views")
 
@@ -9,23 +10,24 @@ get '/articles' do
   CSV.foreach("articles.csv", headers: true, header_converters: :symbol) do |row|
     article = row.to_hash
     @articles << article
+    # binding.pry
   end
   erb :index
 end
-
-# get "article[:article_url]" do
-#
-# end
 
 get '/articles/new' do
   erb :new
 end
 
 post '/articles' do
-
-  CSV.open('articles.csv', 'a') do |csv|
-    csv << [params[:article_title],params[:article_url],params[:article_desc]]
-  end
-
+  CSV.foreach("articles.csv", headers: true, header_converters: :symbol) do |row|
+    if row[:article_url].include?(params[:article_url])
+      redirect '/articles/new'
+    else
+      CSV.open('articles.csv', 'a+') do |csv|
+        csv << [params[:article_title],params[:article_url],params[:article_desc]]
+      end
+    end
   redirect '/articles'
+  end
 end
